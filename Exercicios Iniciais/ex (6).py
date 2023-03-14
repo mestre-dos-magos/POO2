fd35b884ffbb66547f22bc4018f4fbb762d02152
mestre-dos-magos <matheusdvm.pessoal@gmail.com>
2023-03-14 09:56:02 -0300

    ex 6 finish

--- Exercicios Iniciais/ex (6).py
+++ Exercicios Iniciais/ex (6).py
@@ -1,9 +1,12 @@
-def main():
-    pass
-
-
-
+def um_a_vinte_lado_lado():
+    for a in range(1, 21):
+        print(a, end=' ')
+def um_a_vinte_baixo_baixo():
+    for a in range(1,21):
+        print(a, end='\n')
 
 if __name__ == '__main__':
-    main()
+    um_a_vinte_baixo_baixo()
+    um_a_vinte_lado_lado()
+
     
