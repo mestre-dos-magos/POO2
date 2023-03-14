ec4198ed745692dd20f49965b21b971aeb12a59e
mestre-dos-magos <matheusdvm.pessoal@gmail.com>
2023-03-14 09:44:16 -0300

    ex 4 finish

--- Exercicios Iniciais/ex (4).py
+++ Exercicios Iniciais/ex (4).py
@@ -1,8 +1,11 @@
 def main():
-    pass
-
-
-
+    ano, pop_a, pop_b = 0, 80000, 200000
+    while pop_a < pop_b:
+        pop_a *= 1.03
+        pop_b *= 1.015
+        ano += 1
+    #print(ano)
+    return ano
 
 if __name__ == '__main__':
     main()
