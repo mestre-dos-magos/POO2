a7cb57a252bb965a1a392192073a2bfc02ec0a94
mestre-dos-magos <matheusdvm.pessoal@gmail.com>
2023-03-14 10:34:01 -0300

    ex 12-13 finish

--- Exercicios Iniciais/ex (12).py
+++ Exercicios Iniciais/ex (12).py
@@ -1,9 +1,7 @@
-def main():
-    pass
-
-
-
+def tabuada(num=1):
+    for mult in range(1,11):
+        print(f'{num} X {mult} = {int(num*mult)}')
 
 if __name__ == '__main__':
-    main()
+    tabuada()
     
--- Exercicios Iniciais/ex (13).py
+++ Exercicios Iniciais/ex (13).py
@@ -1,9 +1,7 @@
-def main():
-    pass
-
-
-
+def main(base=1,expo=1):
+    #print(base**expo)
+    return base**expo
 
 if __name__ == '__main__':
-    main()
+    main(2,3)
     
